bf32a9afde17d5176eb368eec8e0cdf5bfcd21e5
dieromeo <diegoromeo99@icloud.com>
2023-11-14 09:53:27 +0100

    feat: ciclo for e do while

--- snack-4/main.js
+++ snack-4/main.js
@@ -7,9 +7,22 @@ const numeriUtente = [];
 let sommaNumeri = 0;
 let i = 0; // per scorrere l'array
 
-while (sommaNumeri < 50) {
+/* while (sommaNumeri < 50) {
     numeriUtente.push(+prompt('inserisci un numero: ')); // chiedo all'utente numeri
     sommaNumeri += numeriUtente[i];
     console.log(sommaNumeri);
     i++;
-}
+} */
+
+/* for (let y = 0; sommaNumeri < 50; y++) {
+    numeriUtente.push(+prompt('inserisci un numero: ')); // chiedo all'utente numeri
+    sommaNumeri += numeriUtente[y];
+    console.log(sommaNumeri);
+} */
+
+do {
+    numeriUtente.push(+prompt('inserisci un numero: ')); // chiedo all'utente numeri
+    sommaNumeri += numeriUtente[i];
+    console.log(sommaNumeri);
+    i++;
+} while (sommaNumeri < 50);
